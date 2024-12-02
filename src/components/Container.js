08daf0b4af6e185c868fc8cf4d303921f49ce21e
Moon0922 <moonnight0216@gmail.com>
2024-12-02 12:40:01 +0800

    Initialize

---
+++ src/components/Container.js
@@ -0,0 +1,102 @@
+import React, {useEffect, useState, forwardRef, useImperativeHandle} from 'react';
+import Group from './Group';
+
+const Container = forwardRef(({cardsInfo, curIndex, setCurIndex, curSubIndex, setCurSubIndex, setModalShow}, ref) => {
+    const [translateX, setTranslateX] = useState(0);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [maxTranslateX, setMaxTranslateX] = useState(0);
+
+    useImperativeHandle(ref, () => setTranslateX);
+
+    useEffect(() => {
+        let initialTranslateX = 0;
+        for (let i = 0; i < cardsInfo.length - 1; i++) {
+            initialTranslateX += 336 + 480 * cardsInfo[i].cardInfo.length;
+        }
+        initialTranslateX += 480 * (cardsInfo[cardsInfo.length - 1].cardInfo.length - 1);
+        setMaxTranslateX(initialTranslateX);
+    }, [cardsInfo]);
+
+    useEffect(() => {
+        let initialTranslateX = 0;
+        let i, j;
+        for (i = 0; i < cardsInfo.length; i++) {
+            initialTranslateX += (i < 3) ? 296 : 500;
+            if (translateX < initialTranslateX) {
+                setCurIndex(i);
+                setCurSubIndex(0);
+                break;
+            }
+            for (j = 0; j < cardsInfo[i].cardInfo.length; j++) {
+                initialTranslateX += 480 * j;
+                if (translateX < initialTranslateX) {
+                    break;
+                }
+            }
+            if (j < cardsInfo[i].cardInfo.length) {
+                setCurIndex(i);
+                setCurSubIndex(j);
+                break;
+            }
+            initialTranslateX += 40;
+        }
+    }, [translateX]);
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setStartX(e.clientX);
+        document.body.style.userSelect = "none";
+    };
+
+    const handleMouseMove = (e) => {
+        if (!isDragging) return;
+        const diff = e.clientX - startX;
+        setStartX(e.clientX);
+        setTranslateX((prev) => Math.min(maxTranslateX, Math.max(0, prev - diff)));
+    };
+
+    const handleScroll = (e) => {
+        setTranslateX((prev) => {
+            const sensitivity = 1.5;
+            const nextTranslateX = prev + e.deltaY * sensitivity;
+            return Math.min(Math.max(nextTranslateX, 0), maxTranslateX);
+        });
+    };
+
+    const handleMouseUp = (e) => {
+        setIsDragging(false);
+        document.body.style.userSelect = "";
+    };
+
+    return (
+
+        <div className="flex flex-col bg-white flex-grow justify-center">
+            <div className="relative flex flex-row h-[610px] ml-[102px] overflow-hidden"
+                 onMouseDown={handleMouseDown}
+                 onMouseMove={handleMouseMove}
+                 onMouseUp={handleMouseUp}
+                 onMouseLeave={handleMouseUp}>
+                <div className="flex flex-row gap-[120px] transition-transform duration-[2000ms]"
+                     style={{
+                         transform: `translateX(-${translateX}px)`
+                     }}>
+                    {cardsInfo.map((card, index) => (
+                        <Group
+                            info={card}
+                            key={index}
+                            cardIndex={index}
+                            curSubIndex={curSubIndex}
+                            curIndex={curIndex}
+                            setCurIndex={setCurIndex}
+                            setCurSubIndex={setCurSubIndex}
+                            setModalShow={setModalShow}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+})
+
+export default Container;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Timeline from './Timeline'
+import Container from './Container';
+import Group from './Group';
+import PulseModal from './PulseModal';
+
+export { Timeline, Container, Group, PulseModal };
